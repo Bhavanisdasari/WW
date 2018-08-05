57929c7b76705eabeb8ef57f94ab4799dac0e964
S <S@192.168.1.136>
2018-08-05 18:14:04 -0400

    File path change

--- WeightWatchers/src/com/ww/readfile/AccessFileElements.java
+++ WeightWatchers/src/com/ww/readfile/AccessFileElements.java
@@ -11,14 +11,14 @@ public class AccessFileElements {
 
 	
 	public static void main(String[] args) throws Exception{
-		File myFile = new File("C:\\Workspace_Aug4\\WeightWatchers\\dataFile.txt");
+		File myFile = new File("C:\\Users\\prajy\\git\\repository2\\WeightWatchers\\dataFile.txt");
 		
 		if(myFile.exists()) {
 			System.out.println("File exists");
 		}else {
 			System.out.println("File does not exists");
 		}
-		FileReader freader=new FileReader("C:\\Workspace_Aug4\\WeightWatchers\\dataFile.txt");
+		FileReader freader=new FileReader("C:\\Users\\prajy\\git\\repository2\\WeightWatchers\\dataFile.txt");
 		BufferedReader br= new BufferedReader(freader);
 		String x="";
 		while ((x=br.readLine()) != null){
